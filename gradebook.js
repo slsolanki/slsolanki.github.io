572cfa88a5df7e7e96291f7f970030fd56e8d5c5
slsolanki <slsolanki@my.waketech.edu>
2025-07-14 22:32:51 -0400

    Create gradebook.js

---
+++ gradebook.js
@@ -0,0 +1,10 @@
+function fetchGradeData() {
+  console.log(fetching grade data...");
+}
+
+function populateGradebook(data) {
+  console.log("Populating gradebook with data:", data);
+}
+
+const gradeData = fetchGradeData();
+populateGradeData(gradeData)
